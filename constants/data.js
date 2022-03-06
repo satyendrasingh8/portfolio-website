3f42039ed57364f26ca7ff0cf17f4156907f2610
satyendrasingh8 <satyendrasinghsss7398@gmail.com>
2022-03-06 23:27:37 +0530

    initial commit

---
+++ constants/data.js
@@ -0,0 +1,119 @@
+const userData = {
+  githubUsername: "satyendrasingh8",
+  name: "Satyendra Singh",
+  designation: "Full-Stack Developer",
+  avatarUrl: "/avatar.png",
+  email: "satyendrasinghsss7398@gmail.com",
+  phone: "+91 8948543408",
+  address: "Chitrakoot, Uttar Pradesh, India.",
+  projects: [
+    {
+      title: "Netflix",
+      link: "https://netflix-clone-5a6dc.web.app/",
+      imgUrl: "/playground.png",
+    },
+    {
+      title: "Portfolio",
+      link: "https://satyendra.webserv4u.com/",
+      imgUrl: "/portfolio.png",
+    },
+    {
+      title: "SnackDown 2021 by CodeChef",
+      link: "https://www.codechef.com/certificates/public/11996a3",
+      imgUrl: "/placeholdertech.png",
+    },
+    {
+      title: "Sports Bike",
+      link: "https://github.com/satyendrasingh8/Sports_Bike",
+      imgUrl: "/tmk.jpg",
+    },
+    {
+      title: "Flutter news application",
+      link: "https://www.linkedin.com/posts/satyendrasingh8_socialmedia-app-flutter-activity-6753029989869907968-4WMN",
+      imgUrl: "/covidrescue.png",
+    }, {
+      title: "Flipkart Quiz Application",
+      link: "https://webserv4u.com/",
+      imgUrl: "/feedmeback.png",
+    },
+    
+    {
+      title: "3D HideAndSeek using reactVR and react360",
+      link: "https://drive.google.com/file/d/1E2Amqfh3NyUIM1yMjC41PtYnSDl5csta/view?usp=sharing",
+      imgUrl: "/algochurn.png",
+    },
+    
+   
+    {
+      title: "Blog Application",
+      link: "https://blog.webserv4u.com/2021/03/nit-trichy.html",
+      imgUrl: "/devmedium.png",
+    },
+  ],
+  about: {
+    title:
+      "I'm a software developer that loves building products and web applications that impact millions of lives",
+    description: [
+      `I've been developing full-stack application since I was 16 years old. I didn't know what full-stack meant at that time because the term was not coined back then. I'm so out of content right now that I'm literally crying writing this text since lorem text is so 2012 now.`,
+      `After learning HTML and struggling with CSS, I came up with a brilliant idea of using bootstrap so that I don't have to style everything by myself and - for obvious reasons - if you knew bootstrap, you were cool.`,
+      `But now, I'm a legend. I've been coding in React, Node.js, Express, MongoDB, MySQL, Bootstrap, Tailwind, React Native, Flutter, C, C++, Java, Python, Rust, PHP, Assembly, blogger, Youtuber, Logo and YouDontKnowShit programming language etc to name a few. Although I barely know the syntax (Psst, Stack overflow!), I consider myself a Ninja developer I'm laughing right now.`,
+    ],
+    currentProject: "Flipkart Quiz Application",
+    currentProjectUrl: "https://webserv4u.com/",
+  },
+  experience: [
+    {
+      title: "Software Developer",
+      company: "ForceBolt Pvt. Ltd.",
+      year: "2022",
+      companyLink: "https://www.forcebolt.com/",
+      desc: "Full Stack Development Services & Solution for Frontend Development UI/UX Design & Development Backend Development HTML & CSS Development CMS Development Server and Hosting Integration",
+    },
+    {
+      title: "Founder and Developer",
+      company: "TailwindMasterKit",
+      year: "2021",
+      companyLink: "https://tailwindmasterkit.com",
+      desc: "Full Stack Development Services & Solution for Frontend Development UI/UX Design & Development Backend Development HTML & CSS Development CMS Development Server and Hosting Integration",
+    },
+    {
+      title: "Founder and Developer",
+      company: "Algochurn",
+      year: "2021",
+      companyLink: "https://algochurn.com",
+      desc: "Your last moment revision friend right before a Tech coding interview. Practice problems and get hired at top Product based companies.",
+    },
+    {
+      title: "Internship",
+      company: "Intel Corporation",
+      year: "2019",
+      companyLink: "https://intel.com",
+      desc: "Developed pothole detection system with OpenCV and computer vision, detects potholes on roads and informs the driver.",
+    },
+    {
+      title: "Graduation",
+      company: "UPES, Dehradun.",
+      year: "2019",
+      companyLink: "https://upes.co.in",
+      desc: "Major in Computer Science and Engineering with a CGPA of 7.0. Nobody asks this but it's okay.",
+    },
+    {
+      title: "High School",
+      company: "St. Vishna RR Nector school Mundera Allahabad, Uttar Pradesh",
+      year: "2015",
+      companyLink: "https://www.facebook.com/St.vishna123/",
+      desc: "Subjects being PCM - I barely survived with 92% marks aggregate. Flex fridays, fellas.",
+    },
+  ],
+  resumeUrl:
+    "https://drive.google.com/file/d/1Cy0c_m2AGhbG3Vw_p_zWCUKJOO1OP9zu/view?usp=sharing",
+  socialLinks: {
+    instagram: "https://www.instagram.com/satyendrasingh_89/",
+    twitter: "https://twitter.com/satyend51668580",
+    linkedin: "https://www.linkedin.com/in/satyendrasingh8/",
+    github: "https://github.com/satyendrasingh8",
+    facebook: "https://facebook.com/",
+  },
+};
+
+export default userData;
