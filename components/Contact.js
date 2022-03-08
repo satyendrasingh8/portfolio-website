120a4a57a11c1370777da9423aa667eb49c72924
satyendrasingh8 <satyendrasinghsss7398@gmail.com>
2022-03-08 19:44:52 +0530

    map

--- components/Contact.js
+++ components/Contact.js
@@ -180,6 +180,8 @@ export default function Contact() {
           </form>
         </div>
       </div>
+      <iframe  src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d57634.8678287791!2d81.81904302194273!3d25.465689738775446!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1646748631116!5m2!1sen!2sin" 
+     class="min-w-full " allowfullscreen="" loading="lazy"></iframe>
     </section>
   );
 }
--- pages/contact.js
+++ pages/contact.js
@@ -6,6 +6,7 @@ export default function contact() {
   return (
     <ContainerBlock>
       <Contact />
+      
     </ContainerBlock>
   );
 }
